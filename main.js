f7f2f5f69a50fe888d24e984448a73af49bfa44b
rldald989 <70861085+rldald989@users.noreply.github.com>
2023-03-08 16:53:56 +0000

    Major visual updates

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 let background = document.getElementById('background');
 let context = background.getContext('2d');
-background.width = 1480;
+background.width = 100;
 background.height = 100;
 
-function sidebar(text)
-{
-    
-}
-
-Text("Home Page");
+context.fillStyle = "red";
+context.fillRect(10, 10, 100, 100);
