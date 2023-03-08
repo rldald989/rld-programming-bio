4a87965f80d4d7fc5d1371970935cdf8e7fc54fe
rldald989 <70861085+rldald989@users.noreply.github.com>
2023-03-07 18:55:57 -0800

    First commit with the website
    website

---
+++ main.js
@@ -0,0 +1,11 @@
+let background = document.getElementById('background');
+let context = background.getContext('2d');
+background.width = 1480;
+background.height = 100;
+
+function sidebar(text)
+{
+    
+}
+
+Text("Home Page");
